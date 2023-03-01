31f99ea86fea09ae6eb30dbd5c99262870e2b7c4
panku-chavan <pankajchavan963@gmail.com>
2023-03-01 16:06:37 +0530

    Second Commit

--- src/components/Resume/ResumeNew.jsx
+++ src/components/Resume/ResumeNew.jsx
@@ -3,12 +3,11 @@ import { AiOutlineDownload } from "react-icons/ai";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import {  pdfjs } from "react-pdf";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 import Particle from "../Particle";
 import pdf from "../../Assets/Pankaj_Chavan.pdf";
 import PankajChavan from "../../Assets/PankajChavan.jpg";
-
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 function ResumeNew() {
   return (
     <div>
