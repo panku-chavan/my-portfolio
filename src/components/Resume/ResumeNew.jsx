d08209b1a24487e79837ce3a22daad60e7a05ca4
panku-chavan <pankajchavan963@gmail.com>
2023-03-01 16:01:06 +0530

    Second Commit

--- src/components/Resume/ResumeNew.jsx
+++ src/components/Resume/ResumeNew.jsx
@@ -1,27 +1,25 @@
-import React, { useState, useEffect } from "react";
+
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+import pdf from "../../Assets/Pankaj_Chavan.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
-import { Document, Page, pdfjs } from "react-pdf";
+import {  pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import PankajChavan from "../../Assets/PankajChavan.jpg";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink =
-  "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+
 
 function ResumeNew() {
-  const [width, setWidth] = useState(1200);
+  
 
-  useEffect(() => {
-    setWidth(window.innerWidth);
-  }, []);
+  
 
   return (
     <div>
       <Container fluid className="resume-section">
-        {/* <Particle />
+        <Particle />
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
@@ -35,9 +33,7 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
+          <img src={PankajChavan} alt="PankajChavan" className="img-fluid w-50"></img>
         </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
@@ -50,9 +46,9 @@ function ResumeNew() {
             <AiOutlineDownload />
             &nbsp;Download CV
           </Button>
-        </Row> */}
+        </Row>
 
-        <h1>This Page is not avilable currently....This is under construction</h1>
+        {/* <h1>This Page is not avilable currently....This is under construction</h1> */}
       </Container>
     </div>
   );
