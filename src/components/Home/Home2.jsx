4ab3b2b707d9f947029088c9503346b667e33d22
panku-chavan <pankajchavan963@gmail.com>
2023-03-01 21:34:50 +0530

    Second Commit

--- src/components/Home/Home2.jsx
+++ src/components/Home/Home2.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/avatar.svg";
+//import myImg from "../../Assets/avatar.svg";
+import profilePic from "../../Assets/profilePic.jpg"
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
@@ -54,7 +55,7 @@ function Home2() {
           </Col>
           <Col md={4} className="myAvtar">
             <Tilt>
-              <img src={myImg} className="img-fluid" alt="avatar" />
+              <img src={profilePic} className="img-fluid shadow rounded-3" alt="avatar" />
             </Tilt>
           </Col>
         </Row>
