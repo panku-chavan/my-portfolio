84171edc3be8c1de4515165f52ccacea873d722e
panku-chavan <pankajchavan963@gmail.com>
2023-03-01 16:38:06 +0530

    Third Commit

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,6 +6,7 @@ import calc from "../../Assets/Projects/calc.png";
 import resumeBuilder from "../../Assets/Projects/resumeBuilder.png";
 import flashcard from "../../Assets/Projects/flashcard.png";
 import crypto from "../../Assets/Projects/crypto.png";
+import myportfolio from "../../Assets/Projects/myportfolio.png";
 
 function Projects() {
   return (
@@ -34,32 +35,43 @@ function Projects() {
             <ProjectCard
               imgPath={flashcard}
               isBlog={false}
-              title="Flashcard Generator"
-              description="Flashcard Generator"
+              title="Flashcard Generator "
+              description="This is a Single Page application created using ReactJs. All responsive design are developed using TailwindCss framework. React icons also used. In this app user can make a flashcard as per there information. This project has three pages CreateFlashcard, MyFlashcard and last FlashcardDetails Page. "
               ghLink="https://github.com/panku-chavan/flashcard-generator"
               demoLink="https://flashcard-generator-15e43.web.app//"
             />
           </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={calc}
-              isBlog={false}
-              title="Simple Calculator"
-              description="This is a Simple React App. This is a plain CSS project. All Mathematical operation works properly."
-              ghLink="https://github.com/panku-chavan/simple-calculator"
-              demoLink="https://panku-chavan.github.io/simple-calculator/"              
-            />
-          </Col>
+          
 
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={resumeBuilder}
               isBlog={false}
               title="Resume Builder"
-              description="Resume builder."
+              description="In this app user can make there resume by simply adding there information and download there resume in pdf format.This is a sipmle react app. The hole app develop with reactJs, React hooks, Design with react bootstrap and manage the state using react redux. This is aslo a Single Page application SPA." 
               ghLink="https://github.com/panku-chavan/resume-builder"
               demoLink="https://panku-chavan.github.io/resume-builder/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={myportfolio}
+              isBlog={false}
+              title="My Portfolio"
+              description="This is my portfolio app. I make this this app using create-react-app. I design this app using bootstrap framework. This is responsive app. In this app i tried to explained myself in breif. I have added my resume and all information about me. I added my all projects or recent work also. "
+              ghLink="https://github.com/panku-chavan/my-portfolio"
+              demoLink="https://panku-chavan.github.io/my-portfolio/"              
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={calc}
+              isBlog={false}
+              title="Simple Calculator"
+              description="This is a Simple React App. This is a plain CSS project. All Mathematical operation works properly. Simple Calculator helps users to calculate simple calculations with great speed and accessibility. This makes it the best solution for quick and simple calculations."
+              ghLink="https://github.com/panku-chavan/simple-calculator"
+              demoLink="https://panku-chavan.github.io/simple-calculator/"              
             />
           </Col>
 
