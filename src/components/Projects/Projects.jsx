b62474a1d6ada059fac8957875918d8d3468ef59
panku-chavan <pankajchavan963@gmail.com>
2023-04-10 16:31:44 +0530

    new project added

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -9,6 +9,7 @@ import crypto from "../../Assets/Projects/crypto.png";
 import myportfolio from "../../Assets/Projects/myportfolio.png";
 import getYTS from "../../Assets/Projects/getYTS.png";
 import myPlan from "../../Assets/Projects/myPlan.png";
+import ecom from "../../Assets/Projects/ecom.jpg"
 
 
 function Projects() {
@@ -45,14 +46,23 @@ function Projects() {
             />
           </Col>
 
-          
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={ecom}
+              isBlog={false}
+              title="E-Commerce"
+              description="This is a e-commerce website. This is a Full stack web aplication or a MERN stack web application same as common ecommerce websites. In this app user can buy product online. This project also have admin as well as user dashboard so shop owner which is a admin cad add product and categories. Both front and backend works properly"
+              ghLink="https://github.com/panku-chavan/e-commerce-app-2023"
+              demoLink="https://ruby-tasty-coyote.cyclic.app/"
+            />
+          </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={resumeBuilder}
               isBlog={false}
               title="Resume Builder"
-              description="In this app user can make there resume by simply adding there information and download there resume in pdf format.This is a sipmle react app. The hole app develop with reactJs, React hooks, Design with react bootstrap and manage the state using react redux. This is aslo a Single Page application SPA." 
+              description="In this app user can make there resume by simply adding there information and download there resume in pdf format.This is a sipmle react app. The hole app develop with reactJs, React hooks, Design with react bootstrap and manage the state using react redux. This is aslo a Single Page application SPA."
               ghLink="https://github.com/panku-chavan/resume-builder"
               demoLink="https://panku-chavan.github.io/resume-builder/"
             />
@@ -64,7 +74,7 @@ function Projects() {
               title="My Portfolio"
               description="This is my portfolio app. I make this this app using create-react-app. I design this app using bootstrap framework. This is responsive app. In this app i tried to explained myself in breif. I have added my resume and all information about me. I added my all projects or recent work also. "
               ghLink="https://github.com/panku-chavan/my-portfolio"
-              demoLink="https://panku-chavan.github.io/my-portfolio/"              
+              demoLink="https://panku-chavan.github.io/my-portfolio/"
             />
           </Col>
           <Col md={4} className="project-card">
@@ -74,7 +84,7 @@ function Projects() {
               title="Simple Calculator"
               description="This is a Simple React App. This is a plain CSS project. All Mathematical operation works properly. Simple Calculator helps users to calculate simple calculations with great speed and accessibility. This makes it the best solution for quick and simple calculations."
               ghLink="https://github.com/panku-chavan/simple-calculator"
-              demoLink="https://panku-chavan.github.io/simple-calculator/"              
+              demoLink="https://panku-chavan.github.io/simple-calculator/"
             />
           </Col>
           <Col md={4} className="project-card">
@@ -84,7 +94,7 @@ function Projects() {
               title="Get Youtube Subscribers"
               description="This is a node express app. This is a API to get youtube subscribers details. There are four API endpoints in this app, '/ ' default route, '/subscribers' route , '/subscribers/names' route, '/subscribers/:id' route. for more details aboute this app please read the README.md."
               ghLink="https://github.com/panku-chavan/get-youtube-subscribers"
-              demoLink="https://get-youtube-subscribers-one.vercel.app/"              
+              demoLink="https://get-youtube-subscribers-one.vercel.app/"
             />
           </Col>
           <Col md={4} className="project-card">
@@ -94,9 +104,10 @@ function Projects() {
               title="My Plan"
               description="This is a mini project for creating our daily plans. Like what can i do today or something else. This is a Front-end project using react and boostrap. This app is live on git-hub pages."
               ghLink="https://github.com/panku-chavan/my-plan"
-              demoLink="https://panku-chavan.github.io/my-plan/"              
+              demoLink="https://panku-chavan.github.io/my-plan/"
             />
           </Col>
+
 
         </Row>
       </Container>
