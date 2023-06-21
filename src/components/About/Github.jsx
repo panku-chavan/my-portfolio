d34456a0559eab11feb5c13c2840f7f8f77059f5
panku-chavan <pankajchavan963@gmail.com>
2023-06-21 16:34:49 +0530

    initial commit

--- src/components/About/Github.jsx
+++ src/components/About/Github.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import GitHubCalendar from "react-github-calendar";
 import { Row } from "react-bootstrap";
 
-
 function Github() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "20px" }}>
