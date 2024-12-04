209647dc4689090203b5e8bc14ebc0f54876eb12
panku-chavan <pankajchavan963@gmail.com>
2024-12-04 15:22:37 +0530

    updated

--- src/components/Certificates/CertificateCard.jsx
+++ src/components/Certificates/CertificateCard.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React from "react";
 import Card from "react-bootstrap/Card";
+import { IoMdDownload } from "react-icons/io";
+
 const CertificateCard = ({ img, title }) => {
     return (
-        <Card className="project-card-view">
+        <Card className="project-card-view position-relative">
             <Card.Body>
-                <Card.Img variant="top" src={img} alt="card-img" />
+                <Card.Img variant="top" src={img} alt="card-img" className="position-relative" />
                 <Card.Title>{title}</Card.Title>
-
+                <a 
+                    href={img} 
+                    download 
+                    className="download-icon-btn"
+                >
+                    <IoMdDownload size={24} />
+                </a>
             </Card.Body>
         </Card>
-    )
-}
+    );
+};
 
-export default CertificateCard
+export default CertificateCard;
